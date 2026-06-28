a041b20d7a5670453324ae7e146cd00d2e2dc8bb
0xrushi <6279035+0xrushi@users.noreply.github.com>
2026-06-27 22:10:26 -0400

    Bump version to 1.2.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.inmoair.xrcompanion.core"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     signingConfigs {
